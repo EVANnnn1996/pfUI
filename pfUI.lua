bbcee8ff64eb2dafbfeea9ecef1140c33b15a8ba
Northend <ywli_1996@163.com>
2026-05-25 20:28:04 +0800

    change default class color

--- pfUI.lua
+++ pfUI.lua
@@ -102,15 +102,15 @@ function pfUI:UpdateColors()
   if pfUI.expansion == "vanilla" then
     -- update table to get unknown colors and blue shamans for vanilla
     RAID_CLASS_COLORS = {
-      ["WARRIOR"] = { r = 0.78, g = 0.61, b = 0.43, colorStr = "ffc79c6e" },
-      ["MAGE"]    = { r = 0.41, g = 0.8,  b = 0.94, colorStr = "ff69ccf0" },
-      ["ROGUE"]   = { r = 1,    g = 0.96, b = 0.41, colorStr = "fffff569" },
-      ["DRUID"]   = { r = 1,    g = 0.49, b = 0.04, colorStr = "ffff7d0a" },
-      ["HUNTER"]  = { r = 0.67, g = 0.83, b = 0.45, colorStr = "ffabd473" },
-      ["SHAMAN"]  = { r = 0.14, g = 0.35, b = 1.0,  colorStr = "ff0070de" },
-      ["PRIEST"]  = { r = 1,    g = 1,    b = 1,    colorStr = "ffffffff" },
-      ["WARLOCK"] = { r = 0.58, g = 0.51, b = 0.79, colorStr = "ff9482c9" },
-      ["PALADIN"] = { r = 0.96, g = 0.55, b = 0.73, colorStr = "fff58cba" },
+      ["WARRIOR"] = { r = 0.87, g = 0.67, b = 0.45, colorStr = "ffdfaa73" },
+      ["MAGE"]    = { r = 0,    g = 0.85, b = 1,    colorStr = "ff00d8ff" },
+      ["ROGUE"]   = { r = 1,    g = 0.95, b = 0.41, colorStr = "fffff368" },
+      ["DRUID"]   = { r = 1,    g = 0.66, b = 0,    colorStr = "ffffa900" },
+      ["HUNTER"]  = { r = 0.67, g = 0.93, b = 0.31, colorStr = "ffabed4f" },
+      ["SHAMAN"]  = { r = 0.04, g = 0.49, b = 0.93, colorStr = "ff0a7ded" },
+      ["PRIEST"]  = { r = 0.99, g = 0.99, b = 0.99, colorStr = "fffcfcfc" },
+      ["WARLOCK"] = { r = 0.6,  g = 0.27, b = 1,    colorStr = "ff9946ff" },
+      ["PALADIN"] = { r = 1,    g = 0.58, b = 0.85, colorStr = "ffff95d8" },
     }
 
     RAID_CLASS_COLORS = setmetatable(RAID_CLASS_COLORS, { __index = function(tab,key)
